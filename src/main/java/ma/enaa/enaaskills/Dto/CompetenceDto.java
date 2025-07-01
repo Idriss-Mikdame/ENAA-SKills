d71261c2ec0f95dcd38547de79188e1a48c06587
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-07-01 14:51:33 +0100

    j'ai fait la cratio dto

---
+++ src/main/java/ma/enaa/enaaskills/Dto/CompetenceDto.java
@@ -0,0 +1,45 @@
+package ma.enaa.enaaskills.Dto;
+
+
+
+public class CompetenceDto  {
+    Long id;
+    String code;
+    String nom;
+    String description;
+
+    public CompetenceDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/ma/enaa/enaaskills/Dto/SousCompetenceDto.java
@@ -0,0 +1,47 @@
+package ma.enaa.enaaskills.Dto;
+
+
+
+
+public class SousCompetenceDto  {
+    Long id;
+    String titre;
+    String description;
+    Long competenceId;
+
+
+    public SousCompetenceDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getCompetenceId() {
+        return competenceId;
+    }
+
+    public void setCompetenceId(Long competenceId) {
+        this.competenceId = competenceId;
+    }
+}
