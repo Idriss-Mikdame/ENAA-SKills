9c2d86fa53dcb27343271c38cf6aa881426b0ae3
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-07-06 12:13:18 +0100

    la creation competence Mapper

--- src/main/java/ma/enaa/enaaskills/Mapper/CompetenceMapper.java
+++ src/main/java/ma/enaa/enaaskills/Mapper/CompetenceMapper.java
@@ -3,6 +3,7 @@ package ma.enaa.enaaskills.Mapper;
 import ma.enaa.enaaskills.Dto.CompetenceDto;
 import ma.enaa.enaaskills.Model.Competence;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CompetenceMapper {
